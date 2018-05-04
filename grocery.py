025195cbe8e65adf5c3ec31f4b54925f97a77b73
evanyui <evanyui@hotmail.com>
2018-05-04 02:11:24 -0400

    added send to fb messenger feature

--- grocery.py
+++ grocery.py
@@ -1,26 +1,42 @@
+from fbchat import Client
+from fbchat.models import *
+import json
+
 path = 'grocery/list.txt'
-class GroceryList:
 
-    def __init__(self):
-        load()
+def __loadCredential(app):
+    with open('credentials/'+app+'.json', 'r') as f:
+        credential = json.load(f)
+    return credential
 
-    def load(self):
-        with open(path, 'r') as f:
-            list = f.readlines()
-        return list
+def load():
+    with open(path, 'r') as f:
+        list = f.read().splitlines()
+    return list
 
-    def add(self, item):
-        with open(path, 'a') as f:
-            f.write(item+'\n')
+def add(item):
+    with open(path, 'a') as f:
+        f.write(item)
+        f.write('\n')
 
-    def reset(self):
-        open(path, 'w').close()
+def reset():
+    open(path, 'w').close()
 
-    def send():
+def send():
     """
     This will use the facebook api to send the grocery list as a message to user
     """
-        pass
-
-
+    try:
+        fb_credential = __loadCredential('fb')
+        client = Client(fb_credential['email'], fb_credential['password'])
+        grocery_list = load()
+        message = "<Sent from A.I管家>\n\nYour Grocery List:\n- {}".format("\n- ".join(grocery_list))
+        client.send(Message(text=message), thread_id=client.uid, thread_type=ThreadType.USER)
+        client.logout()
+        return True
+    except:
+        return False
     
+
+
+
